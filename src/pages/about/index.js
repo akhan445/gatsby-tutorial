6aa9b4ea9c982c19a583b4faabe0cb9c5408d060
Abdullah S. Khan <abdullahshamskhan@gmail.com>
2022-04-05 11:30:39 -0400

    Finished tutorial 3 of gatsby

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Layout from "../../components/layout/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
     <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    <StaticImage
+        alt="logo"
+        src="../../images/icon.png"
+        />
   </Layout>
   )
 }
